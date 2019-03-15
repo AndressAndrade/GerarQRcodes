3bacbed3dbf844ad1aac0f6c1808019cd4fd8feb
Andressa Andrade <dsandrade@dcc.ufba.br>
2019-03-15 09:10:24 -0300

    Update scscs.ts

--- scscs.ts
+++ scscs.ts
@@ -2,66 +2,105 @@ import * as QRCode from 'qrcode';
 import * as text2png from 'text2png';
 import * as fs from 'fs';
 import * as temp from 'fs-temp/promise';
-import * as Zip from 'node-zip';
 import * as Jimp from 'jimp';
+import * as archiver from 'archiver';
 
 let files = '/tmp/qrcode';
-let texto = './texto.png';
-let zip = new Zip();
+let texto = '/tmp/texto.png';
 
-QRCode.toFile(
-    files,
-    'Some text',
-    {
-        scale: 1,
-        width: 600
-    },
-
-    function(err) {
-        if (err) throw err;
-        console.log('done');
-    }
-);
-
-temp.writeFile(
-    text2png('1Ed6s', {
-        font: '160px Futura',
-        color: 'black',
-        backgroundColor: 'white',
-        lineSpacing: 10,
-        paddingBottom: 50,
-        paddingTop: 600,
-        paddingLeft: 150,
-        paddingRight: 150
-    })
-).then(path => {
-    // `path` now holds the path to a file with the specified `data`
-    Jimp.read(path)
-
-        //combine logo into image
-        .then(tpl =>
-            Jimp.read(files)
-                .then(logoTpl => {
-                    return tpl.composite(logoTpl, 100, 2);
-                })
-
-                //export image
-                .then(tpl => {
-                    tpl.quality(100).write('tmp/out.png');
-                    //zip.file('ousdt', tpl['data']);
-                    console.log(Jimp['data']);
-                })
-
-                //catch errors
-                .catch(err => {
-                    console.error(err);
-                })
-        );
+let output = fs.createWriteStream(__dirname + '/example.zip');
+let archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
 });
 
-//dsssssssssssssssssssssssssssssss
+// listen for all archive data to be written
+// 'close' event is fired only when a file descriptor is involved
+output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log(
+        'archiver has been finalized and the output file descriptor has closed.'
+    );
+});
 
-//read template & clone raw image
+// This event is fired when the data source is drained no matter what was the data source.
+// It is not part of this library but rather from the NodeJS Stream API.
+// @see: https://nodejs.org/api/stream.html#stream_event_end
+output.on('end', function() {
+    console.log('Data has been drained');
+});
 
-// const data = zip.generate({ base64: false, compression: 'DEFLATE' });
-// fs.writeFileSync('test.zip', data, 'binary');
+// good practice to catch warnings (ie stat failures and other non-blocking errors)
+archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+        // log warning
+    } else {
+        // throw error
+        throw err;
+    }
+});
+
+// good practice to catch this error explicitly
+archive.on('error', function(err) {
+    throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+
+for (let i = 0; i < 4; i++) {
+    QRCode.toFile(
+        files,
+        'Some text',
+        {
+            scale: 1,
+            width: 600
+        },
+
+        function(err) {
+            if (err) throw err;
+            console.log('done');
+        }
+    );
+
+    temp.writeFile(
+        text2png('2Vz5d', {
+            font: '160px FiraMono',
+            localFontPath: 'font/FiraMono-Regular.ttf',
+            localFontName: 'FiraMono',
+            color: 'black',
+            backgroundColor: 'white',
+            lineSpacing: 10,
+            paddingBottom: 50,
+            paddingTop: 600,
+            paddingLeft: 150,
+            paddingRight: 150
+        })
+    ).then(path => {
+        // `path` now holds the path to a file with the specified `data`
+        Jimp.read(path)
+
+            //combine logo into image
+            .then(tpl =>
+                Jimp.read(files)
+                    .then(logoTpl => {
+                        return tpl.composite(logoTpl, 100, 2);
+                    })
+
+                    //export image
+                    .then(tpl => {
+                        tpl.quality(100).write('tmp/out.png');
+                    })
+
+                    //catch errors
+                    .catch(err => {
+                        console.error(err);
+                    })
+            );
+    });
+
+    let indiceName = 'out_' + i + '.png';
+    // append a file
+    archive.file('tmp/out.png', { name: indiceName });
+}
+
+archive.finalize();
